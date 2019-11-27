d6d2e6b03d24dbf0a4fe8fc55a6aa53e7cfbd68f
phoenix-Force <paradox.joe.007@gmail.com>
2019-11-27 19:39:17 +0530

    initial commit

---
+++ components/Navigation.vue
@@ -0,0 +1,91 @@
+<template>
+
+<b-container fluid>
+  <sidebar-menu :menu="menu" />
+  <b-navbar sticky vertical toggleable="lg" type="dark" variant="dark">
+    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse">
+
+    </b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="#">Home</b-nav-item>
+        <b-nav-item href="#" disabled>Gallery</b-nav-item>
+        <b-nav-item href="#" disabled>Timeline</b-nav-item>
+        <b-nav-item-dropdown
+          id="my-nav-dropdown"
+          text="Others"
+          toggle-class="nav-link-custom"
+          right
+          variant="dark"
+        >
+          <b-dropdown-item>Events</b-dropdown-item>
+          <b-dropdown-item>Reminder</b-dropdown-item>
+          <b-dropdown-divider></b-dropdown-divider>
+          <b-dropdown-item>Three</b-dropdown-item>
+        </b-nav-item-dropdown>
+        <b-nav-item href="#" disabled>Others</b-nav-item>
+        <b-nav-item href="#" disabled>Bussiness</b-nav-item>
+        <b-nav-item href="#" disabled>Disabled</b-nav-item>
+        <b-nav-item href="#" disabled>Disabled</b-nav-item>
+      </b-navbar-nav>
+
+      Right aligned nav items
+      <b-navbar-nav class="ml-auto">
+
+        <b-nav-item-dropdown text="Lang" right>
+          <b-dropdown-item href="#">EN</b-dropdown-item>
+          <b-dropdown-item href="#">ES</b-dropdown-item>
+          <b-dropdown-item href="#">RU</b-dropdown-item>
+          <b-dropdown-item href="#">FA</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown right>
+          <template v-slot:button-content>
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</b-container>
+</template>
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+      menu:[
+        {
+          header: true,
+          title: 'Main Navigation',
+          hiddenOnCollapse: true
+        },
+        {
+          href: '/',
+          title: 'Dashboard',
+          icon: 'fas fa-home'
+        },
+        {
+          href: '/charts',
+          title: 'Charts',
+          icon: 'fa fa-chart-area',
+          child: [
+            {
+              href: '/charts/sublink',
+              title: 'Sub Link'
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,99 @@
+<template>
+  <v-app id="sandbox">
+    <v-navigation-drawer
+      v-model="primaryDrawer.model"
+      :clipped="primaryDrawer.clipped"
+      :floating="primaryDrawer.floating"
+      :mini-variant="primaryDrawer.mini"
+      :permanent="primaryDrawer.type === 'permanent'"
+      :temporary="primaryDrawer.type === 'temporary'"
+      app
+      overflow
+    >
+    <v-row class="kk">
+      <v-col cols="12">
+        <v-row
+          v-ripple
+          class="text-center elevation-2 text-center"
+
+          style="min-height: 60px;color:white;text-align-center"
+        >
+        <a class="text-center">Home</a>
+
+        </v-row>
+      </v-col>
+
+    </v-row>
+
+    </v-navigation-drawer>
+
+    <v-app-bar
+      :clipped-left="primaryDrawer.clipped"
+      absolute
+      app
+    >
+      <v-app-bar-nav-icon
+        v-if="primaryDrawer.type !== 'permanent'"
+        @click.stop="primaryDrawer.model = !primaryDrawer.model"
+      />
+      <v-toolbar-title>Paradox</v-toolbar-title>
+      <v-switch
+        v-model="$vuetify.theme.dark"
+          primary
+          label="Dark"
+          class="float-right"
+      />
+    </v-app-bar>
+
+
+    <v-content>
+      <v-container fluid>
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col cols="12">
+            <!-- <h5></!--> -->
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+
+
+
+
+    <v-footer
+      :inset="footer.inset"
+      app
+    >
+      <!-- <span class="px-4">&copy; {{ new Date().getFullYear() }}</span> -->
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      drawers: ['Default (no property)', 'Permanent', 'Temporary'],
+      primaryDrawer: {
+        model: null,
+        type: 'default (no property)',
+        clipped: false,
+        floating: false,
+        mini: false,
+      },
+      footer: {
+        inset: false,
+      },
+    }),
+  }
+
+  // :collapse="!collapseOnScroll"
+  //     :collapse-on-scroll="collapseOnScroll"
+</script>
+<style>
+
+.kk{
+  background-color:primary;
+}
+</style>
